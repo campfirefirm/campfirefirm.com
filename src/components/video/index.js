a517d09af760254b475f354d302ef4f5cfd779da
Bran Sorem <contact@bransorem.com>
2020-09-21 11:55:02 -0700

    WIP videos

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -2,20 +2,21 @@ import React, { useState } from 'react'
 import './styles.scss'
 import ModalVideo from 'react-modal-video'
 
-const videos = [
-  '456286137',
-  '283818650/e7f7aaa9f1',
-  '199230971',
-  '372215739/4a0851b86a',
-  '219768695',
-  '456292098/af2715d8a7',
-  '380062204',
-  '381715174/fe0c564563',
-];
-
 const Video = () => {
   const [open, setOpen] = useState(false);
   const [id, setId] = useState(null);
+
+  React.useEffect(() => {
+    if (!open) {
+      setId(null);
+    }
+  }, [open]);
+
+  React.useEffect(() => {
+    if (id) {
+      setOpen(true);
+    }
+  }, [id]);
 
   return (
     <section id="video">
@@ -28,26 +29,40 @@ const Video = () => {
       <div className="content cols">
         <div className="col">
           <p>With over a decade of production experience Campfire is ready to help you promote your product, campaign, or cause. From script to final video - we’ll be with you every step of the way.</p>
-          <p><b>Let’s get started on your video today!</b></p>
+          <p><b>Let’s start creating your video today!</b></p>
         </div>
         <div className="col">
           <p>We have experience with:</p>
           <ul>
-            <li><button onClick={ () => { setId('456286137'); setOpen(true); } }>Climate policy</button></li>
-            <li><button onClick={ () => { setId('283818650/e7f7aaa9f1'); setOpen(true); } }>Candidate campaigns</button></li>
-            <li><button onClick={ () => { setId('199230971'); setOpen(true); } }>Broadcast TV ads</button></li>
-            <li><button onClick={ () => { setId('372215739/4a0851b86a'); setOpen(true); } }>Local ballot measures</button></li>
-            <li><button onClick={ () => { setId('219768695'); setOpen(true); } }>Animated videos</button></li>
-            <li><button onClick={ () => { setId('456292098/af2715d8a7'); setOpen(true); } }>Digital ads</button></li>
-            <li><button onClick={ () => { setId('380062204'); setOpen(true); } }>Live streams</button></li>
-            <li><button onClick={ () => { setId('381715174/fe0c564563'); setOpen(true); } }>And more...</button></li>
+            <li>
+              <button onClick={ () => setId('456286137') }>Climate policy</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('283818650/e7f7aaa9f1') }>Candidate campaigns</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('199230971') }>Broadcast TV ads</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('372215739/4a0851b86a') }>Local ballot measures</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('219768695') }>Animated videos</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('456292098/af2715d8a7') }>Digital ads</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('380062204') }>Live streams</button>
+            </li>
+            <li>
+              <button onClick={ () => setId('381715174/fe0c564563') }>And more...</button>
+            </li>
           </ul>
         </div>
       </div>
 
-      { videos.map(video => (
-        <ModalVideo channel='vimeo' isOpen={ videos.indexOf(id) >= 0 && open } videoId={video} onClose={() => setOpen(false) } />
-      ))}
+      <ModalVideo channel='vimeo' isOpen={ open } videoId={id} onClose={() => setOpen(false) } />
     </section>
   );
 };

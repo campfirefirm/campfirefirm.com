dbffb199528234707235d45d09a9a98d1557b788
Bran Sorem <contact@bransorem.com>
2020-09-21 11:17:28 -0700

    WIP videos

--- src/components/video/index.js
+++ src/components/video/index.js
@@ -1,56 +1,21 @@
-import React, { useEffect } from 'react'
-import Vimeo from '@u-wave/react-vimeo';
-import './styles.scss';
-import styled from 'styled-components';
-
-const VideoContainer = styled.div`
-  flex-direction: row;
-  justify-content: space-around;
-`;
+import React, { useState } from 'react'
+import './styles.scss'
+import ModalVideo from 'react-modal-video'
 
 const videos = [
-  'https://www.vimeo.com/456286137',
-  'https://www.vimeo.com/283818650/e7f7aaa9f1',
-  'https://www.vimeo.com/199230971',
-  'https://www.vimeo.com/372215739/4a0851b86a',
-  'https://www.vimeo.com/219768695',
-  'https://www.vimeo.com/456292098/af2715d8a7',
-  'https://www.vimeo.com/381715174/fe0c564563',
+  '456286137',
+  '283818650/e7f7aaa9f1',
+  '199230971',
+  '372215739/4a0851b86a',
+  '219768695',
+  '456292098/af2715d8a7',
+  '380062204',
+  '381715174/fe0c564563',
 ];
 
-const VideoCard = (props) => {
-  const { src } = props;
-
-  return (
-    <Vimeo
-      video={src}
-      showPortrait={false}
-      showTitle={false}
-      showByline={false}
-      // height={'200px'}
-      style={{
-        margin: '0 1rem',
-        maxWidth: '100%',
-      }}
-    />
-  );
-}
-
-const StyledVideo = styled(VideoCard)``;
-
-const Carousel = () => {
-  return (
-    <VideoContainer className="content cols" style={{ overflowX: 'scroll', padding: 0, margin: 0 }}>
-      {videos.map( video => (
-        <StyledVideo src={video} />
-      ))}
-    </VideoContainer>
-  )
-}
-
 const Video = () => {
-  useEffect(() => {
-  }, []);
+  const [open, setOpen] = useState(false);
+  const [id, setId] = useState(null);
 
   return (
     <section id="video">
@@ -59,19 +24,6 @@ const Video = () => {
         <div className="video shadow">
           <iframe title="video" width="560" height="315" src="https://www.youtube.com/embed/bVBWmBj1fQo" frameBorder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
         </div>
-        {/* <Carousel /> */}
-      </div>
-      <div className="content cols">
-        <div className="col"><VideoCard src='https://www.vimeo.com/456286137' /></div>
-        <div className="col"><VideoCard src='https://www.vimeo.com/283818650/e7f7aaa9f1' /></div>
-      </div>
-      <div className="content cols">
-        <div className="col"><VideoCard src='https://www.vimeo.com/199230971' /></div>
-        <div className="col"><VideoCard src='https://www.vimeo.com/372215739/4a0851b86a' /></div>
-      </div>
-      <div className="content cols">
-      <div className="col"><VideoCard src='https://www.vimeo.com/456292098/af2715d8a7' /></div>
-        <div className="col"><VideoCard src='https://www.vimeo.com/219768695' /></div>
       </div>
       <div className="content cols">
         <div className="col">
@@ -81,16 +33,21 @@ const Video = () => {
         <div className="col">
           <p>We have experience with:</p>
           <ul>
-            <li>Policy rollouts</li>
-            <li>Candidate campaigns</li>
-            <li>Ballot measures</li>
-            <li>Animated videos</li>
-            <li>Digital ads</li>
-            <li>Live Streams</li>
-            <li>And more... </li>
+            <li><button onClick={ () => { setId('456286137'); setOpen(true); } }>Climate policy</button></li>
+            <li><button onClick={ () => { setId('283818650/e7f7aaa9f1'); setOpen(true); } }>Candidate campaigns</button></li>
+            <li><button onClick={ () => { setId('199230971'); setOpen(true); } }>Broadcast TV ads</button></li>
+            <li><button onClick={ () => { setId('372215739/4a0851b86a'); setOpen(true); } }>Local ballot measures</button></li>
+            <li><button onClick={ () => { setId('219768695'); setOpen(true); } }>Animated videos</button></li>
+            <li><button onClick={ () => { setId('456292098/af2715d8a7'); setOpen(true); } }>Digital ads</button></li>
+            <li><button onClick={ () => { setId('380062204'); setOpen(true); } }>Live streams</button></li>
+            <li><button onClick={ () => { setId('381715174/fe0c564563'); setOpen(true); } }>And more...</button></li>
           </ul>
         </div>
       </div>
+
+      { videos.map(video => (
+        <ModalVideo channel='vimeo' isOpen={ videos.indexOf(id) >= 0 && open } videoId={video} onClose={() => setOpen(false) } />
+      ))}
     </section>
   );
 };
